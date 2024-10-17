d00554903e38206fbfa5470f88dfe96f7e9e8cf2
Sevo <sevodo.dessau@gmail.com>
2024-10-17 15:49:33 +0200

    add mobile menu with fades

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -1,31 +1,48 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 
 export default function MobileMenu({triggerContent,children}) {
     const [overlayVisible, setOverlayVisible] = useState(false);
+    let overlayRef = useRef(null);
+
 
     useEffect(() => {
-        console.log("overlayVisible:", overlayVisible);
         if(overlayVisible) {
+            setOverlayVisible(true);
+            //overlayRef.current.classList.add("fade-in");
+            overlayRef.current.classList.remove("fade-out");
+            overlayRef.current.classList.add("fade-in");
+            console.log(overlayRef.current);
             document.body.style.overflowY = "hidden";
         }else {
+
             document.body.style.overflowY = "auto";
+
         }
     }, [overlayVisible]);
 
 
+
+
     function triggerHandler(e) {
         e.preventDefault();
-        setOverlayVisible((currentValue) => {
-            return !currentValue;
-        });
+        if(!overlayVisible) {
+            setOverlayVisible(true);
+        }else {
+            overlayRef.current.classList.remove("fade-in");
+            overlayRef.current.classList.add("fade-out");
+            setTimeout(() => {
+                setOverlayVisible(false);
+            }, 150);
+        }
+        //setOverlayVisible((currentValue) => !currentValue);
         console.log("mobile trigger clicked");
     }
     return (
         
         <div className="MobileMenu">
             {overlayVisible && (
-                <div className="MobileMenuOverlay absolute left-0 top-0 bottom-0 right-0 bg-black opacity-95 z-50">
+                <div ref={overlayRef} className="MobileMenuOverlay absolute left-0 top-0 bottom-0 right-0 bg-black opacity-95 z-50">
                     <div className="w-full h-full flex justify-center items-center">
                         {children}
                     </div>
@@ -39,4 +56,8 @@ export default function MobileMenu({triggerContent,children}) {
             </button>  
         </div>
     );
+}
+
+const style = {
+    backgroundColor: "red"
 }
