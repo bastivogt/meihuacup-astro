3cf39da208282754efe210362ca437ebe3915a21
Sevo <sevodo.dessau@gmail.com>
2024-10-18 20:50:28 +0200

    add news detail

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -56,7 +56,7 @@ export default function MobileMenu({children}) {
                 </div>
             )}
             <button onClick={triggerHandler}>
-                <svg className="transition-all ease-linear duration-200 fill-grey-lighter hover:fill-white" xmlns="http://www.w3.org/2000/svg" height="60px" viewBox="0 -960 960 960" width="60px" fill="#ffffff"><path d="M120-240v-66.67h720V-240H120Zm0-206.67v-66.66h720v66.66H120Zm0-206.66V-720h720v66.67H120Z"/></svg>
+                <svg className="transition-all ease-linear duration-200 fill-white hover:fill-grey-lighter" xmlns="http://www.w3.org/2000/svg" height="60px" viewBox="0 -960 960 960" width="60px" fill="#ffffff"><path d="M120-240v-66.67h720V-240H120Zm0-206.67v-66.66h720v66.66H120Zm0-206.66V-720h720v66.67H120Z"/></svg>
             </button>  
         </div>
     );
